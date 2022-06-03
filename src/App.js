e904cbad15ef0e18f4603872093c90a1d156ab06
groovebass <groovebassist1@gmail.com>
2022-06-03 14:46:42 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import Directory from './components/directory/directory.component';
+
+const App = () => {
+
+  const categories = [
+    {
+      id: 1,
+      title: 'Basses',
+      imageUrl: 'https://res.cloudinary.com/demawo/image/upload/v1654258145/demawo_store_media/bass_btnznu.jpg',
+      
+    },
+    {
+      id: 2,
+      title: 'Drums',
+      imageUrl: 'https://res.cloudinary.com/demawo/image/upload/v1654258148/demawo_store_media/drums_yxol3l.jpg',
+      
+    },
+    {
+      id: 3,
+      title: 'Guitars',
+      imageUrl: 'https://res.cloudinary.com/demawo/image/upload/v1654259352/demawo_store_media/guitar_utdb4r.jpg',
+      
+    },
+    {
+      id: 4,
+      title: 'Pianos',
+      imageUrl: 'https://res.cloudinary.com/demawo/image/upload/v1654258147/demawo_store_media/piano_xv1sjy.jpg',
+      
+    },
+    {
+      id: 5,
+      title: 'Studio',
+      imageUrl: 'https://res.cloudinary.com/demawo/image/upload/v1654259352/demawo_store_media/headphones_a4pugn.jpg',
+      
+    }
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Directory categories={categories}/>
   );
 }
 
